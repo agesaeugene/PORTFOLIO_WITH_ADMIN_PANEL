0827eee1bfbaa61a0d31e21a72204f7e83c97ac6
Eugene Agesa <eugenethebandit@gmail.com>
2026-02-10 23:33:58 +0300

    added vite base path url for my frontend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ dotenv.config({ path: "./config/config.env" });
 app.use(
   cors({
     origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
 );
